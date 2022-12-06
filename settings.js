5dfa7ec5d0abd00b5543f5167e825f43eccf8008
dickmans <30838551+dickmans@users.noreply.github.com>
2022-12-06 10:36:14 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+// Fusion 360 Manage connection based on Forge App
+exports.clientId        = '';
+exports.clientSecret    = '';
+exports.redirectUri     = 'http://localhost:8080/callback';
+exports.port            = 8080;
+exports.tenant          = '';
