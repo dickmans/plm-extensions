4ab6f27ef4e5a41356b1debea1296806e8ce374d
dickmans <sven.dickmans@autodesk.com>
2023-09-13 19:13:27 +0200

    Enhancement: Enable setting file selection

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let fileSettings = (process.argv.length > 2) ? '.' + process.argv[2] : '';
 
-
 const express     = require('express');
 const session     = require('express-session');
 const path        = require('path');
