f3cd7d5aaa56eede83aee03ad6270104045d3ba6
Sven Dickmans <sven.dickmans@autodesk.com>
2024-03-08 10:57:28 +0100

    [DOC : Developer Guide] Update to Navigation Views contents

--- public/javascripts/framework/docs.js
+++ public/javascripts/framework/docs.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('.nav-content').click(function() {
         $('.doc-content').hide();
         $('#' + $(this).attr('data-id')).show();
+        $('#doc-contents').animate({ scrollTop: 0 }, 250);
     });
 
     $('.nav-content').first().click();
