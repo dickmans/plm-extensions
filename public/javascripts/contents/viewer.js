bc0a08dec9187df74923083481891f1400d794f3
Sven Dickmans <sven.dickmans@autodesk.com>
2024-04-08 22:43:38 +0200

    [UPD : Viewer] New optional controls (fit all visible, fit first)

--- public/javascripts/contents/viewer.js
+++ public/javascripts/contents/viewer.js
@@ -204,22 +204,22 @@ function onViewerGeometryLoaded() {
 }
 function setViewerFeatures() {
 
-    if (typeof features === 'undefined') {
+    if (Object.keys(applicationFeatures).length === 0) {
         viewer.toolbar.setVisible(true);
         return;
     }
 
-    if(isBlank(features)) return;
-    if(isBlank(features.viewer)) return;
+    if(isBlank(applicationFeatures)) return;
+    if(isBlank(applicationFeatures.viewer)) return;
 
-    for(let feature of Object.keys(features.viewer)) {
+    for(let applicationFeature of Object.keys(applicationFeatures.viewer)) {
 
-        if(features.viewer[feature] === false) {
+        if(applicationFeatures.viewer[applicationFeature] === false) {
 
             let elemParent = null;
             let controlId  = '';
 
-            switch(feature) {
+            switch(applicationFeature) {
 
                 case 'orbit'        : elemParent = viewer.toolbar.getControl('navTools');       controlId = 'toolbar-orbitTools'; break;
                 case 'firstPerson'  : elemParent = viewer.toolbar.getControl('navTools');       controlId = 'toolbar-bimWalkTool'; break;
@@ -242,12 +242,14 @@ function setViewerFeatures() {
                     
         } else {
 
-            switch(feature) {
+            switch(applicationFeature) {
                 
                 case 'markup'    : viewerAddMarkupControls();  break;
                 case 'reset'     : viewerAddResetButton();     break;
                 case 'ghosting'  : viewerAddGhostingToggle();  break;
                 case 'highlight' : viewerAddHighlightToggle(); break;
+                case 'fitToView' : viewerAddFitToView();       break;
+                case 'single'    : viewerAddFitFirstInstance();break;
                 case 'views'     : viewerAddViewsToolbar();    break;
 
             }
@@ -255,8 +257,8 @@ function setViewerFeatures() {
         }      
     }
 
-    if(!isBlank(features.viewer.cube)) {
-        if(features.viewer.cube) $('body').removeClass('no-viewer-cube');
+    if(!isBlank(applicationFeatures.viewer.cube)) {
+        if(applicationFeatures.viewer.cube) $('body').removeClass('no-viewer-cube');
     }
 
     viewer.toolbar.setVisible(true);
@@ -409,13 +411,16 @@ function viewerSelectModels(partNumbers, params) {
     }
 
     for(let dataInstance of dataInstances) {
+        let isSelected = false;
         for(let partNumber of partNumbers) {
             if(dataInstance.partNumber === partNumber) {
                 dbIds.push(dataInstance.dbId);
                 viewer.show(dataInstance.dbId);
+                isSelected = true;
                 if(highlight) viewer.setThemingColor(dataInstance.dbId, color, null, true );
             }
         }
+        dataInstance.selected = isSelected;
     }
 
     if(ghosting)  viewer.setGhosting(true);
@@ -553,17 +558,20 @@ function viewerResetSelection(params) {
     //  Set defaults for optional parameters
     // --------------------------------------
     let fitToView   = true;     // Zoom in / out to fit all into view
+    let resetView   = false;    // Reset view to initial view from file
     let resetColors = true;     // Highlight given partNumber(s) by defined color (colorModelSelected)
 
     if( isBlank(params)            )      params = {};
     if(!isBlank(params.fitToView)  )   fitToView = params.fitToView;
+    if(!isBlank(params.resetView)  )   resetView = params.resetView;
     if(!isBlank(params.resetColors)) resetColors = params.resetColors;
 
     viewer.showAll();
     viewer.clearSelection();
     
-    if(resetColors) viewer.clearThemingColors();
-    if(fitToView  ) viewer.setViewFromFile();
+         if(resetColors) viewer.clearThemingColors();
+         if(resetView  ) viewer.setViewFromFile();
+    else if(fitToView  ) viewer.fitToView();
 
 }
 
@@ -1039,6 +1047,60 @@ function toggleSelectionHighlight(enabled) {
             viewer.setThemingColor(dataInstance.dbId, colorModelSelected, null, true );
         }
     }
+
+}
+
+
+// Custom Controls : Fit (selection) to view
+function viewerAddFitToView() {
+    
+    let customToolbar = new Autodesk.Viewing.UI.ControlGroup('custom-toolbar-fit');
+
+    let buttionFitToView = addCustomControl(customToolbar, 'button-fit-to-view', 'icon-viewer', 'Fit (selected) components to view');
+        buttionFitToView.onClick = function() { 
+
+            let dbIds = [];
+
+            for(let dataInstance of dataInstances) {
+                if(viewer.isNodeVisible(dataInstance.dbId)) {
+                    dbIds.push(dataInstance.dbId);
+                }
+            }
+
+            viewer.fitToView(dbIds);
+
+        };
+        
+    viewer.toolbar.addControl(customToolbar);
+
+}
+
+
+// Custom Controls : 
+function viewerAddFitFirstInstance() {
+
+    let customToolbar = new Autodesk.Viewing.UI.ControlGroup('custom-toolbar-single');
+
+    let buttionFitFirst = addCustomControl(customToolbar, 'button-fit-first', 'icon-first', 'Fit first instance to view');
+        buttionFitFirst.onClick = function() { 
+
+            let dbIdFirst = [];
+
+            for(let dataInstance of dataInstances) {
+                if(dataInstance.selected === true) {
+                    if(!isBlank(dataInstance.name)) {
+                        if(dbIdFirst.length === 0) {
+                            dbIdFirst.push(dataInstance.dbId);
+                        }
+                    }
+                }
+            }
+
+            viewer.fitToView(dbIdFirst);
+            
+        };
+        
+    viewer.toolbar.addControl(customToolbar);
 
 }
 
