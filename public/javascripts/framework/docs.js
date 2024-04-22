79a79622b6729edb24bb4786740677cd3d98cb8c
Sven Dickmans <sven.dickmans@autodesk.com>
2024-04-22 17:30:27 +0200

    [DOC : Tutorials] Initial version of Tutorial #1

--- public/javascripts/framework/docs.js
+++ public/javascripts/framework/docs.js
@@ -15,6 +15,49 @@ $(document).ready(function() {
         $('#doc-contents').animate({ scrollTop: 0 }, 250);
     });
 
+    $('span.ref').click(function() {
+        $('.doc-content').hide();
+        $('#' + $(this).attr('data-id')).show();
+        $('#doc-contents').animate({ scrollTop: 0 }, 250);
+    });
+
     $('.nav-content').first().click();
 
+    updateLinks();
+
 });
+
+
+function updateLinks() {
+
+    let location = document.location.href.split('/docs');
+
+    $('a.button').each(function() {
+
+        let href = $(this).attr('href');
+
+        if(href.indexOf('youtu.be') < 0) {
+
+            let url = location[0] + '/' + href;
+            if($(this).html() === '') $(this).html(url);
+            let concat = (url.indexOf('?') > -1) ? '&' : '?';
+            if(location.length > 1)url += concat + location[1];
+            $(this).attr('href', url);
+            
+        }
+
+    });
+
+    $('.code').each(function() {
+
+        let text = $(this).html();
+        text = text.replace(/LOCATION/g, location[0]);
+        $(this).html(text);
+
+    });
+
+    $('.url').each(function() {
+        $(this).html(location[0]);
+    });
+
+}
--- public/javascripts/tutorial/1-template.js
+++ public/javascripts/tutorial/1-template.js
@@ -1,40 +1,5 @@
 $(document).ready(function() {
     
-    let link = '/api/v3/workspaces/' + wsId + '/items/' + dmsId;
-    
-    // call methods of common/utils.js to insert DOM elements
-    appendProcessing('attachments', false);
-    appendProcessing('details', false);
-    appendViewerProcessing();
-    appendOverlay();
-    
-    // call methods of common/ui.js to insert PLM data
-    insertViewer(link);
-    insertBOM(link);
-    insertItemDetails(link);
-    insertAttachments(link);
-
-    // get selected item's descriptor and insert in header subtitle
-    setHeaderSubtitle(link);
-
-    // define user interaction events
-    setUIEvents();
+    insertMOW();
 
 });
-
-function setHeaderSubtitle(link) {
-
-    $.get('/plm/descriptor', { 'link' : link }, function(response) {
-        $('#header-subtitle').html(response.data);
-    });
-
-}
-
-function setUIEvents() {
-
-    // Header Toolbar
-    $('#button-settings').click(function() {
-        showErrorMessage('Expected Error', 'Not implemented yet');
-    });
-
-}
