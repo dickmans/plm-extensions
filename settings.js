bda465315b119c52362378090bc7147f5df6c172
avolger <167975855+avolger@users.noreply.github.com>
2024-09-16 11:48:42 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // Fusion 360 Manage connection based on APS Application
-let clientId        = '';
+let clientId        = '6x2wGJAOsgfcjNvVwH2uQCspWWGBLA9bjhYv20UFiGkymhHA';
 // let clientSecret    = '';
 let tenant          = 'hawacloudsandbox';
 let redirectUri     = 'http://localhost:8080/callback';
