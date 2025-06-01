be3c890d0749d375a93109a816ad26724081db13
Sven Dickmans <sven.dickmans@autodesk.com>
2025-06-01 20:39:41 +0200

    [UPD : Framework] New file to insert adminControls

---
+++ public/javascripts/contents/admin.js
@@ -0,0 +1,111 @@
+// Inser PLM Users selector
+function insertUsers(params) {
+
+    if(isBlank(params)) params = {};
+
+    let id = isBlank(params.id) ? 'users' : params.id;
+
+    let panelSettings = getPanelSettings('', params, {
+        headerLabel : 'Users',
+        contentSize : 'm',
+        layout      : 'table'
+    }, [ ]);
+
+    settings.users[id] = panelSettings;
+    settings.users[id].load = function() { insertUsersData(id); }
+
+    genPanelTop(id, panelSettings, 'users');
+    genPanelHeader(id, panelSettings);
+    genPanelSelectionControls(id, panelSettings);
+    genPanelSearchInput(id, panelSettings);
+    genPanelResizeButton(id, panelSettings);
+    genPanelReloadButton(id, panelSettings);
+    genPanelContents(id, panelSettings);
+
+    insertUsersDone(id);
+
+    panelSettings.load();
+
+}
+function insertUsersDone(id) {}
+function insertUsersData(id) {
+
+    let panelSettings = settings.users[id];
+        panelSettings.timestamp = startPanelContentUpdate(id);
+
+    $.get('/plm/users', { 
+        bulk       : false,
+        activeOnly : true, 
+        mappedOnly : true,
+        timestamp  : panelSettings.timestamp
+    }, function(response) {
+
+        if(stopPanelContentUpdate(response, panelSettings)) return;
+        
+        for(let user of response.data.items) {
+            user.displayName = (user.displayName === ' ') ? user.email : user.displayName;
+        }
+
+        let users = [];
+
+        let columns = [
+            { displayName : 'User'         , fieldId : 'user'        },
+            { displayName : 'First Name'   , fieldId : 'firstName'   },
+            { displayName : 'Last Name'    , fieldId : 'lastName'    },
+            { displayName : 'Organization' , fieldId : 'organization'},
+            { displayName : 'Address'      , fieldId : 'address'     },
+            { displayName : 'City'         , fieldId : 'city'        },
+            { displayName : 'Country'      , fieldId : 'country'     },
+            { displayName : 'Last Login'   , fieldId : 'lastLogin'   },
+            { displayName : 'Image'        , fieldId : 'image'       }
+        ]
+
+        for(let column of columns) {
+            if(includePanelTableColumn(column.displayName, panelSettings, panelSettings.columns.length)) {
+                panelSettings.columns.push(column);
+            }
+        }
+
+        sortArray(response.data.items, 'displayName');
+
+        for(let user of response.data.items) {
+
+            if(!user.tenantAdmin) {
+
+                let lastLogin = new Date(user.lastLoginTime);
+                let image     = '';
+                
+                if(!isBlank(user.image)) {
+                    image = '<img src="' + user.image.large + '">';
+                }
+
+                users.push({
+                    link     : user.email,
+                    image    : user.image.large,
+                    title    : user.displayName,
+                    subtitle : '',
+                    details  : '',
+                    data     : [
+                        { fieldId : 'user', value : user.displayName},
+                        { fieldId : 'firstName', value : user.firstName},
+                        { fieldId : 'lastName', value : user.lastName},
+                        { fieldId : 'organization', value : user.organization},
+                        { fieldId : 'country', value : user.country},
+                        { fieldId : 'city', value : user.city},
+                        { fieldId : 'address', value : user.address1},
+                        { fieldId : 'lastLogin', value : lastLogin.toLocaleDateString()},
+                        { fieldId : 'image', value : image}
+                    ]
+                });
+
+            }
+
+        }
+
+        finishPanelContentUpdate(id, panelSettings, users);
+        insertUsersDataDone(id, response);
+
+    })
+
+}
+function insertUsersDataDone(id, response) {};
