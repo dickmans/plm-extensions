10ea35c37197bf681ca0e2873c0cb826e911b5b3
Sven Dickmans <sven@dickmans.de>
2026-03-09 13:50:10 +0100

    [BUG : Framework] Fixed issue opening the Start menu from /landing

--- public/javascripts/framework/landing.js
+++ public/javascripts/framework/landing.js
@@ -78,9 +78,14 @@ $(document).ready(function() {
         $(this).toggleClass('icon-tiles').toggleClass('icon-tiles-list');
         $('body').toggleClass('layout-tiles-list');
     });    
+
     $('#start').click(function() {
         let href = document.location.href.split('?');
-        let url  = href[0] += 'start?theme=' + $('#theme-selector').val();
+        let base = href[0].split('/landing')[0];
+
+        if(!base.endsWith('/')) base += '/';
+
+        let url  = base += 'start?theme=' + $('#theme-selector').val();
 
         document.location.href = url;
 
