bc470041c00b1ec4a7ed80c56925e619bff43e5c
Sven Dickmans <sven@dickmans.de>
2026-04-24 14:59:20 +0200

    [UPD : Studio] Enhanced panel configuration studio

--- public/javascripts/dev/studio.js
+++ public/javascripts/dev/studio.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
     
     $('.panel-type').first().click();
 
+    if(!isBlank(urlParameters.panel)) {
+        $('.panel-type').each(function() {
+            if($(this).attr('data-id') == urlParameters.panel) {
+                $(this).click();
+                if(!isBlank(urlParameters.run)) {
+                    if(urlParameters.run == 'true') {
+                        runPanel();
+                    }
+                }
+            }
+        });
+    }
+
 });
 
 
@@ -79,105 +92,137 @@ function selectPanelType(elemClicked) {
 
     for(let panelType of listTypes) {
         if(panelType.id === id) {
-            setPanelTypeInputsAndSettings(panelType);
+            setPanelTypeInputsAndOptions(panelType);
         }
     }
 
 }
-function setPanelTypeInputsAndSettings(panelType) {
+function setPanelTypeInputsAndOptions(panelType) {
 
     $('#settings-title'      ).html(panelType.title);
     $('#settings-description').html(panelType.description);
     $('#settings-function'   ).html(panelType.function);
 
-    let elemInputs = $('#settings-inputs').html('');
+    let elemInputs  = $('#settings-inputs' ).html('');
+    let elemOptions = $('#settings-options').html('');
 
-    for(let input of panelType.inputs) {
+    if(!panelType.hasOwnProperty('inputs')) $('#settings-inputs').addClass('hidden');
+    else if(panelType.inputs.length  === 0) $('#settings-inputs').addClass('hidden');
+    else                                    $('#settings-inputs').removeClass('hidden');
+
+    for(let panelInput of panelType.inputs) {
 
         let elemInput = $('<div></div>').appendTo(elemInputs)
             .addClass('panel-input');
 
         $('<div></div>').appendTo(elemInput)
             .addClass('panel-input-title')
-            .html(input.title);
+            .html(panelInput.title);
 
-        if(input.required) elemInput.addClass('required');
+        if(panelInput.required) elemInput.addClass('required');
 
-        switch(input.type) {
+        switch(panelInput.type) {
 
             case 'string':
                 let elemString = $('<input></input>').appendTo(elemInput)
                     .addClass('panel-input-control')
-                if(input.hasOwnProperty('default')) elemString.val(input.default);
+                if(panelInput.hasOwnProperty('default')) elemString.val(panelInput.default);
                 break;
 
             case 'textarea':
                 let elemTextarea = $('<textarea></textarea>').appendTo(elemInput)
                     .addClass('panel-input-control')
-                if(input.hasOwnProperty('default')) elemTextarea.val(input.default);
+                if(panelInput.hasOwnProperty('default')) elemTextarea.val(panelInput.default);
                 break;
 
         }
 
         $('<div></div>').appendTo(elemInput)
+            .addClass('nowrap')
             .addClass('panel-input-description')
-            .html(input.description);
+            .html(panelInput.description);
 
     }
 
-    for(let panelSetting of panelType.common) {
-        let commonSetting = commonSettings[panelSetting[0]];
-        let defaultValue  = ((panelSetting.length > 1)) ? panelSetting[1] : (commonSetting.hasOwnProperty('default')) ? commonSetting.default : null;
-        insertPanelSettingControl('common', commonSetting, defaultValue, commonSetting.options);
-    }
+    insertCommonOptions(panelType, 'Header Options', headerOptions);
+    insertCommonOptions(panelType, 'Content Options', contentOptions);
 
-    if(panelType.hasOwnProperty('special')) {
-        for(let specialSetting of panelType.special) {
-            insertPanelSettingControl('special', specialSetting, specialSetting.default,specialSetting.options);
-        }
-    }
+    // for(let panelOption of panelType.options) {
+    //     let commonSetting = commonOptions[panelOption[0]];
+    //     let defaultValue  = ((panelOption.length > 1)) ? panelOption[1] : (commonSetting.hasOwnProperty('default')) ? commonSetting.default : null;
+    //     insertPanelOptionControl('common', commonSetting, defaultValue, commonSetting.options);
+    // }
+
+    // if(panelType.hasOwnProperty('special')) {
+    //     for(let specialSetting of panelType.special) {
+    //         insertPanelOptionControl('special', specialSetting, specialSetting.default,specialSetting.options);
+    //     }
+    // }
 
     setJSONString();
 
 }
-function insertPanelSettingControl(className, panelSetting, defaultValue, listOptions) {
+function insertCommonOptions(panelType, title, options) {
 
-    let elemInputs = $('#settings-inputs');
+    $('<div></div>').appendTo($('#settings-options'))
+        .addClass('settings-category')
+        .html(title)
 
-    let elemSetting = $('<div></div>').appendTo(elemInputs)
-        .addClass('panel-setting')
+    let keys = Object.keys(options);
+
+    for(let key of keys) {
+
+        let option = options[key];
+
+        if(panelType.options.includes(key)) {
+
+            let defaultValue  =  (option.hasOwnProperty('default')) ? option.default : null;
+            if(panelType.defaults.hasOwnProperty(key)) defaultValue = panelType.defaults[key]; 
+            insertCommonOptionControl('common', key, option, defaultValue);
+
+        }
+
+    }
+
+}
+function insertCommonOptionControl(className, key, commonOption, defaultValue) {
+
+    let elemOptions = $('#settings-options');
+
+    let elemOption = $('<div></div>').appendTo(elemOptions)
+        .addClass('panel-option')
         .addClass('panel-' + className);
 
-    $('<div></div>').appendTo(elemSetting)
-        .addClass('panel-setting-title')
-        .html(panelSetting.title);
+    $('<div></div>').appendTo(elemOption)
+        .addClass('panel-option-title')
+        .html(commonOption.title);
 
     let elemControl;
 
-    switch(panelSetting.type) {
+    switch(commonOption.type) {
 
         case 'string':
-            elemControl = $('<input></input>').appendTo(elemSetting)
+            elemControl = $('<input></input>').appendTo(elemOption)
                 .on('keyup', function() {setJSONString(); })
             if(defaultValue !== null) elemControl.val(defaultValue);
             break;
 
         case 'integer':
-            elemControl = $('<input></input>').appendTo(elemSetting)
+            elemControl = $('<input></input>').appendTo(elemOption)
                 .attr('type', 'number')
                 .on('keyup', function() {setJSONString(); })
             if(defaultValue !== null) elemControl.val(defaultValue);
             break;
 
         case 'textarea':
-            elemControl = $('<textarea></textarea>').appendTo(elemSetting)
+            elemControl = $('<textarea></textarea>').appendTo(elemOption)
                 .on('keyup', function() {setJSONString(); })
             if(input.hasOwnProperty('default')) elemTextarea.val(input.default);
             break;
 
         case 'boolean':
-            elemControl = $('<div></div>').appendTo(elemSetting)
-                .addClass('panel-setting-checkbox')
+            elemControl = $('<div></div>').appendTo(elemOption)
+                .addClass('panel-option-checkbox')
                 .addClass('icon')
                 .addClass('icon-check-box')
                 .attr('type', 'checkbox')
@@ -195,10 +240,10 @@ function insertPanelSettingControl(className, panelSetting, defaultValue, listOp
             break;
 
         case 'select':
-            elemControl = $('<select></select>').appendTo(elemSetting)
+            elemControl = $('<select></select>').appendTo(elemOption)
                 .on('change', function() {setJSONString(); })
                 
-            for(let option of listOptions) {
+            for(let option of commonOption.list) {
                 $('<option></option>').appendTo(elemControl).attr('value', option).html(option);
             }
             if(defaultValue !== null) elemControl.val(defaultValue);
@@ -207,13 +252,13 @@ function insertPanelSettingControl(className, panelSetting, defaultValue, listOp
 
     }
 
-    elemControl.attr('data-id', panelSetting.id).addClass('panel-setting-control').addClass('button');
+    elemControl.attr('data-id', key).addClass('panel-option-control').addClass('button');
 
-    $('<div></div>').appendTo(elemSetting)
-        .addClass('panel-setting-description')
+    $('<div></div>').appendTo(elemOption)
+        .addClass('panel-option-description')
         .addClass('nowrap')
-        .attr('title', panelSetting.description)
-        .html(panelSetting.description);
+        .attr('title', commonOption.description)
+        .html(commonOption.description);
 
 }
 
@@ -224,7 +269,7 @@ function runPanel() {
     let count        = $('.panel-input-control').length;
     let params       = { id : 'panel' }
 
-    $('.panel-setting-control').each(function() {
+    $('.panel-option-control').each(function() {
         let elemControl = $(this);
         let type = elemControl.attr('type');
         switch(type) {
@@ -264,7 +309,7 @@ function setJSONString() {
 
     let params = {}
 
-    $('.panel-setting-control').each(function() {
+    $('.panel-option-control').each(function() {
         let elemControl = $(this);
         let type = elemControl.attr('type');
         switch(type) {
